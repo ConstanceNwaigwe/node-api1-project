db390e99e2e22340fef5ccebf4a223aba47c62d2
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-10-28 00:55:28 -0400

    added post request

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+server.post('/api/users', (req, res) => {
+    res.status(400).json({ message: "Please provide name and bio for the user" });
+})
