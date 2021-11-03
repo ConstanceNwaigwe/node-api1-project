ebc55d4259169fae945f295dcf91ac1bed3a683c
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-03 16:22:42 -0400

    added delete request for selected user

--- api/server.js
+++ api/server.js
@@ -48,6 +48,29 @@ server.get("/api/users/:id", (req,res) => {
         })
     })
 })
+server.delete("/api/users/:id", async (req,res) => {
+    try{
+        const selectedUser = await User.findById(req.params.id);
+        if(!selectedUser){
+            res.status(404).json({ 
+                message: "The user with the specified ID does not exist" })
+        } else{
+            const deletedUser = await remove(selectedUser.id);
+            res.status(200).json(deletedUser);
+        }
+     } catch (err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        })}
+})
+server.put("/api/users/:id", async (req,res) => {
+    try{
+
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified"
+        })}
+})
 /*server.use('*', (req,res) => {
     res.status(404).json({
         message: 'nope'
