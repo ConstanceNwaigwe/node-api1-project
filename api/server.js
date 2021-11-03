65395a03dc5eeeef43f258432c91f037d2b4a34e
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-03 16:05:06 -0400

    added post request for new user

--- api/server.js
+++ api/server.js
@@ -2,6 +2,26 @@
 const express = require('express');
 const server = express();
 const User = require('./users/model');
+
+server.use(express.json());
+
+server.post('/api/users', (req,res) => {
+    const user = req.body;
+    if(!user.name || user.bio){
+        res.status(400).json({ 
+            message: "Please provide name and bio for the user" });
+    }
+    User.insert(user)
+    .then(newuser => {
+        res.status(201).json(newuser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved"
+        });
+    })
+})
+
 server.get("/api/users", (req,res) => {
     User.find()
     .then(users => {
@@ -10,7 +30,7 @@ server.get("/api/users", (req,res) => {
     .catch(err => {
         res.status(500).json({
             message: "The users information could not be retrieved"
-        })
+        });
     })
 })
 server.get("/api/users/:id", (req,res) => {
