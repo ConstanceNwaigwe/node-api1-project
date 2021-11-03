a43e931180478de50df49e4e38b7e958166de729
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-03 15:52:14 -0400

    added get request for users and user id

--- api/server.js
+++ api/server.js
@@ -1,4 +1,37 @@
 // BUILD YOUR SERVER HERE
-//const express = require('express');
+const express = require('express');
+const server = express();
+const User = require('./users/model');
+server.get("/api/users", (req,res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved"
+        })
+    })
+})
+server.get("/api/users/:id", (req,res) => {
+    User.findById()
+    .then(user => {
+        res.json(user)
+        if(!user){
+            res.status(404).json({ 
+                message: "The user with the specified ID does not exist" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user information could not be retrieved"
+        })
+    })
+})
+/*server.use('*', (req,res) => {
+    res.status(404).json({
+        message: 'nope'
+    })
+})*/
 
-module.exports = 'express'; // EXPORT YOUR SERVER instead of {}
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-server.post('/api/users', (req, res) => {
-    res.status(400).json({ message: "Please provide name and bio for the user" });
-})
+/*server.post('/api/users', (req, res) => {
+    const id = req.params.id;
+    res.status(201).json({newUser: req.body, id: id});
+    res.status(500).json({ message: "There was an error while saving the user to the database" });
+    res.status(400).json({ message: "Please provide name and bio for the user" })
+});*/
+
+server.listen(port, (() => {
+    console.log("list");
+}))
